c9e9ca4713826324ba823940aad6584aa187b7ca
Suphakit-Somwanich <suphakit_somwanich@cmu.ac.th>
2020-03-24 19:30:49 +0700

    Updates

---
+++ precache-manifest.44781352c1afc9d9f75c2c441cb71fc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c757ac30a7980618068a4fa2220b468",
+    "url": "/Hw8/index.html"
+  },
+  {
+    "revision": "6d2dddb8a80b19878e7a",
+    "url": "/Hw8/static/css/main.4851e9fc.chunk.css"
+  },
+  {
+    "revision": "034cb3a96342ab3727c4",
+    "url": "/Hw8/static/js/2.e2d1c825.chunk.js"
+  },
+  {
+    "revision": "9cc59aa88bbb965336535eb6b360773d",
+    "url": "/Hw8/static/js/2.e2d1c825.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d2dddb8a80b19878e7a",
+    "url": "/Hw8/static/js/main.e07374e6.chunk.js"
+  },
+  {
+    "revision": "1d656dc15cb21d40ff95",
+    "url": "/Hw8/static/js/runtime-main.71cb8a15.js"
+  }
+]);
